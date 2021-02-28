564c23141a126cc11c5ca079f898f622f7a080e5
parthojit <parthojit@yahoo.com>
2021-02-28 13:57:29 +0900

    test

--- counter.js
+++ counter.js
@@ -1,11 +1,8 @@
 var count = 0;
 var button = document.getElementById('clickme');
 
-
 button.onclick = function() {
-
   count = count + 5;
   button.innerHTML = "You Deposited: " + count + " min";
-
 };
 
